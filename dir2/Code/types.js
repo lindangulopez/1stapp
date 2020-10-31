bdfb63d87693ae499463e6ff664be356bd8c5ed2
LINDA ANGULO LOPEZ <43523986+lindangulopez@users.noreply.github.com>
2020-10-31 20:02:01 +0100

    types js

---
+++ dir2/Code/types.js
@@ -0,0 +1,21 @@
+int x = 3;
+double d=x; //implicit conversion
+
+//explicit cast
+
+double d= 3.14;
+int x = (int)d;
+
+//method calls
+
+String s ="3";
+int x = Integer.parseInt(s);
+
+///PRIMITIVE///////////////
+//int, double, char, boolean
+//1float, long, byte, short
+
+///OBJECTS////////////////
+//String, Point, Shape, 
+//a Class
+//.method()
