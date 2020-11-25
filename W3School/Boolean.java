eb48b701c6f6222aee3f9850da01ae6e51125f2d
LINDA ANGULO LOPEZ <43523986+lindangulopez@users.noreply.github.com>
2020-11-25 20:14:21 +0100

    Java Booleans

---
+++ W3School/Boolean.java
@@ -0,0 +1,32 @@
+//create
+public class Main {
+  public static void main(String[] args) {
+    boolean isJavaFun = true;
+    boolean isFishTasty = false;    
+    System.out.println(isJavaFun);
+    System.out.println(isFishTasty);
+  }
+}
+
+
+/*
+
+//Query
+public class Main {
+  public static void main(String[] args) {
+    int x = 10;
+    int y = 9;
+    System.out.println(x > y); // returns true, because 10 is higher than 9  
+  }
+}
+
+//check
+public class Main {
+  public static void main(String[] args) {
+    int x = 10;
+    System.out.println(x == 10); // returns true, because the value of x is equal to 10
+  }
+}
+
+
+*/
