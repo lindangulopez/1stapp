27ec2bfddb9e12076bba1b4c481de5a437b325a2
LINDA ANGULO LOPEZ <43523986+lindangulopez@users.noreply.github.com>
2020-11-25 20:07:11 +0100

    Java Strings

---
+++ W3School/Strings.java
@@ -0,0 +1,67 @@
+
+//create
+
+public class Main {
+  public static void main(String[] args) {
+    String greeting = "Hello";
+    System.out.println(greeting);
+  }
+}
+
+/*
+
+//quotes
+public class Main {
+  public static void main(String[] args) {
+    String txt1 = "It's alright";
+    String txt2 = "That's great";
+    System.out.println(txt1);
+    System.out.println(txt2);
+  }
+}
+
+//get Length
+public class Main {
+  public static void main(String[] args) {
+    String txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    System.out.println("The length of the txt string is: " + txt.length());
+  }
+}
+
+//Change Case
+public class Main {
+  public static void main(String[] args) {
+    String txt = "Hello World";
+    System.out.println(txt.toUpperCase());
+    System.out.println(txt.toLowerCase());
+  }
+}
+
+//index Location
+public class Main {
+  public static void main(String[] args) {
+    String txt = "Please locate where 'locate' occurs!";
+    System.out.println(txt.indexOf("locate"));
+  }
+}
+
+//Concatenate
+public class Main {
+  public static void main(String args[]) {
+    String firstName = "John";
+    String lastName = "Doe";
+    System.out.println(firstName + " " + lastName);
+  }
+}
+
+//string beats numbers
+public class Main {
+  public static void main(String[] args) {
+    String x = "10";
+    int y = 20;
+    String z = x + y;
+    System.out.println(z);
+  }
+}
+
+*/
