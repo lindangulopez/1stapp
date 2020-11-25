f54913f62a3bef3d01747569f9ffb7701c9ea23f
LINDA ANGULO LOPEZ <43523986+lindangulopez@users.noreply.github.com>
2020-11-25 19:51:19 +0100

    Java Data Types

---
+++ W3School/DataTypes.java
@@ -0,0 +1,95 @@
+public class Main {
+  public static void main(String[] args) {
+    int myNum = 5;               // integer (whole number)
+    float myFloatNum = 5.99f;    // floating point number
+    char myLetter = 'D';         // character
+    boolean myBool = true;       // boolean
+    String myText = "Hello";     // String    
+    System.out.println(myNum);
+    System.out.println(myFloatNum);
+    System.out.println(myLetter);
+    System.out.println(myBool);
+    System.out.println(myText);
+  }
+}
+
+/*
+
+//byte number
+public class Main {
+  public static void main(String[] args) {
+    byte myNum = 100;
+    System.out.println(myNum);  
+  }
+}
+
+//short type
+public class Main {
+  public static void main(String[] args) {
+    short myNum = 5000;
+    System.out.println(myNum);  
+  }
+}
+
+//interger tpe
+public class Main {
+  public static void main(String[] args) {
+    int myNum = 100000;
+    System.out.println(myNum);  
+  }
+}
+
+//Long Number
+public class Main {
+  public static void main(String[] args) {
+    long myNum = 15000000000L;
+    System.out.println(myNum);  
+  }
+}
+
+//float type
+public class Main {
+  public static void main(String[] args) {
+    float myNum = 5.75f;
+    System.out.println(myNum);  
+  }
+}
+
+//double type
+public class Main {
+  public static void main(String[] args) {
+    double myNum = 19.99d;
+    System.out.println(myNum);  
+  }
+}
+
+//Boolean Type
+
+public class Main {
+  public static void main(String[] args) {
+    boolean isJavaFun = true;
+    boolean isFishTasty = false;    
+    System.out.println(isJavaFun);
+    System.out.println(isFishTasty);
+  }
+}
+
+//Character Type
+public class Main {
+  public static void main(String[] args) {
+    char myGrade = 'B';
+    System.out.println(myGrade);
+  }
+}
+//String Type
+
+public class Main {
+  public static void main(String[] args) {
+    String greeting = "Hello World";
+    System.out.println(greeting);
+  }
+}
+
+
+
+*/
