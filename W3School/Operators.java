9e7626d110286f2982a4093cd436bf584ad6bce3
LINDA ANGULO LOPEZ <43523986+lindangulopez@users.noreply.github.com>
2020-11-25 20:00:58 +0100

    Java Operators

---
+++ W3School/Operators.java
@@ -0,0 +1,93 @@
+//Add
+
+public class Main {
+  public static void main(String[] args) {
+    int x = 5;
+    int y = 3;
+    System.out.println(x + y);
+  }
+}
+
+/*
+
+//subtract
+public class Main {
+  public static void main(String[] args) {
+    int x = 5;
+    int y = 3;
+    System.out.println(x - y);
+  }
+}
+
+//multi^ply
+public class Main {
+  public static void main(String[] args) {
+    int x = 5;
+    int y = 3;
+    System.out.println(x * y);
+  }
+}
+
+//divide
+public class Main {
+  public static void main(String[] args) {
+    int x = 12;
+    int y = 3;
+    System.out.println(x / y);
+  }
+}
+
+//Modulus
+public class Main {
+  public static void main(String[] args) {
+    int x = 5;
+    int y = 2;
+    System.out.println(x % y);
+  }
+}
+
+//increment by 1
+public class Main {
+  public static void main(String[] args) {
+    int x = 5;
+    ++x;
+    System.out.println(x);
+  }
+}
+
+//decrement by 1
+public class Main {
+  public static void main(String[] args) {
+    int x = 5;
+    --x;
+    System.out.println(x);
+  }
+}
+
+//Assign, =
+public class Main {
+  public static void main(String[] args) {
+    int x = 5;
+    System.out.println(x);
+  }
+}
+
+//Compare, is the same as
+public class Main {
+  public static void main(String[] args) {
+    int x = 5;
+    int y = 3;
+    System.out.println(x == y); // returns false because 5 is not equal to 3
+  }
+}
+
+
+//logical, AND
+public class Main {
+  public static void main(String[] args) {
+    int x = 5;
+    System.out.println(x > 3 && x < 10); // returns true because 5 is greater than 3 AND 5 is less than 10
+  }
+}
+
+*/
