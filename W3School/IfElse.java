1d7347dad755698b86393cf9dc9c3a87aabae4a9
LINDA ANGULO LOPEZ <43523986+lindangulopez@users.noreply.github.com>
2020-11-25 20:16:45 +0100

    Java If...Else (Conditions)

---
+++ W3School/IfElse.java
@@ -0,0 +1,40 @@
+//IF
+
+public class Main {
+  public static void main(String[] args) {
+    if (20 > 18) {
+      System.out.println("20 is greater than 18"); // obviously
+    }  
+  }
+}
+
+/*
+
+//If ELSE
+
+public class Main {
+  public static void main(String[] args) {
+    int time = 20;
+    if (time < 18) {
+      System.out.println("Good day.");
+    } else {
+      System.out.println("Good evening.");
+    }  
+  }
+}
+
+//ELSE IF
+public class Main {
+  public static void main(String[] args) {
+    int time = 22;
+    if (time < 10) {
+      System.out.println("Good morning.");
+    } else if (time < 20) {
+      System.out.println("Good day.");
+    }  else {
+      System.out.println("Good evening.");
+    }
+  }
+}
+
+*/
