31bcfe8b2c24338bfb9cb0fec3520369912372ee
LINDA ANGULO LOPEZ <43523986+lindangulopez@users.noreply.github.com>
2020-11-25 19:53:45 +0100

    Java Type Casting

---
+++ W3School/TypeCasting.java
@@ -0,0 +1,26 @@
+//Int to Double
+
+public class Main {
+  public static void main(String[] args) {
+    int myInt = 9;
+    double myDouble = myInt; // Automatic casting: int to double
+
+    System.out.println(myInt);
+    System.out.println(myDouble);
+  }
+}
+
+/*
+
+//Double to Int
+public class Main {
+  public static void main(String[] args) {
+    double myDouble = 9.78;
+    int myInt = (int) myDouble; // Explicit casting: double to int
+
+    System.out.println(myDouble);
+    System.out.println(myInt);
+  }
+}
+
+*/
