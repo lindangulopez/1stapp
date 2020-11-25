53c0b67689d94dd35ac242270d88d4b61c285514
LINDA ANGULO LOPEZ <43523986+lindangulopez@users.noreply.github.com>
2020-11-25 19:43:08 +0100

    Java Variables

--- W3School/MainStaticVoids.java
+++ W3School/MainStaticVoids.java
@@ -4,3 +4,70 @@ public class Main {
     System.out.println(name);
   }
 }
+
+/* 
+
+public class Main {
+  public static void main(String[] args) {
+    int myNum = 15;
+    System.out.println(myNum);
+  }
+}
+
+//assign value afterwards
+public class Main {
+  public static void main(String[] args) {
+    int myNum;
+    myNum = 15;
+    System.out.println(myNum);
+  }
+}
+
+//overwrite existing
+
+public class Main {
+  public static void main(String[] args) {
+    int myNum = 15;
+    myNum = 20;  // myNum is now 20
+    System.out.println(myNum);
+  }
+}
+
+//create a final unchangable read only variable
+
+public class Main {
+  public static void main(String[] args) {
+    final int myNum = 15;
+    myNum = 20; // will generate an error
+    System.out.println(myNum);
+  }
+}
+
+//combine text & variables
+public class Main {
+  public static void main(String[] args) {
+    String name = "Mary";
+    String name2 = "Jane";
+    System.out.println("Hello " + name + name2);
+  }
+}
+
+//add variables
+public class Main {
+  public static void main(String[] args) {
+    String firstName = "John ";
+    String lastName = "Doe";
+    String fullName = firstName + lastName;
+    System.out.println(fullName);  
+  }
+}
+
+//simple math
+public class Main {
+  public static void main(String[] args) {
+    int x = 5, y = 6, z = 50;
+    System.out.println(x + y + z);
+  }
+}
+
+*/
