1086182bf1e2fc158ecbb6cc3c68ed5b33a2bf1a
LINDA ANGULO LOPEZ <43523986+lindangulopez@users.noreply.github.com>
2020-11-25 21:03:08 +0100

    Recursion Example

---
+++ W3School/Recursion.java
@@ -0,0 +1,28 @@
+/*
+Recursion Example
+Adding two numbers together is easy to do, but adding a range of numbers is more complicated. In the following example,
+recursion is used to add a range of numbers together by breaking it down into the simple task of adding two numbers:
+10 + sum(9)
+10 + ( 9 + sum(8) )
+10 + ( 9 + ( 8 + sum(7) ) )
+...
+10 + 9 + 8 + 7 + 6 + 5 + 4 + 3 + 2 + 1 + sum(0)
+10 + 9 + 8 + 7 + 6 + 5 + 4 + 3 + 2 + 1 + 0
+
+*/
+
+public class Main {
+  public static void main(String[] args) {
+    int result = sum(10);
+    System.out.println(result);
+  }
+  public static int sum(int k) {
+    if (k > 0) {
+      return k + sum(k - 1);
+    } else {
+      return 0;
+    }
+  }
+}
+
+//Since the function does not call itself when k is 0, the program stops there and returns the result.
