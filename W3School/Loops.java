7650040b456022bb5577c94b8fd16c2e637cf335
LINDA ANGULO LOPEZ <43523986+lindangulopez@users.noreply.github.com>
2020-11-25 20:24:12 +0100

    Java Loops

---
+++ W3School/Loops.java
@@ -0,0 +1,73 @@
+
+//while
+
+public class Main {
+  public static void main(String[] args) {
+    int i = 0;
+    while (i < 5) {
+      System.out.println(i);
+      i++;
+    }  
+  }
+}
+
+/*
+//do while
+public class Main {
+  public static void main(String[] args) {
+    int i = 0;
+    do {
+      System.out.println(i);
+      i++;
+    }
+    while (i < 5);  
+  }
+}
+
+//for
+public class Main {
+  public static void main(String[] args) {
+    for (int i = 0; i < 5; i++) {
+      System.out.println(i);
+    }  
+  }
+}
+
+//Foe Each
+public class Main {
+  public static void main(String[] args) {
+    String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+    for (String i : cars) {
+      System.out.println(i);
+    }    
+  }
+}
+
+//if break
+
+public class Main {
+  public static void main(String[] args) {
+    for (int i = 0; i < 10; i++) {
+      if (i == 4) {
+        break;
+      }
+      System.out.println(i);
+    }  
+  }
+}
+
+//if continue
+public class Main {
+  public static void main(String[] args) {
+    for (int i = 0; i < 10; i++) {
+      if (i == 4) {
+        continue;
+      }
+      System.out.println(i);
+    }  
+  }
+}
+
+
+
+*/
