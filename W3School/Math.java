1574bfe33eb068784aa9da67e796abad78a40fbd
LINDA ANGULO LOPEZ <43523986+lindangulopez@users.noreply.github.com>
2020-11-25 20:12:01 +0100

    Java Math

---
+++ W3School/Math.java
@@ -0,0 +1,36 @@
+//max
+public class Main {
+  public static void main(String[] args) {
+    System.out.println(Math.max(5, 10));  
+  }
+}
+
+/*
+//min
+public class Main {
+  public static void main(String[] args) {
+    System.out.println(Math.min(5, 10));  
+  }
+}
+//square root
+public class Main {
+  public static void main(String[] args) {
+    System.out.println(Math.sqrt(64));  
+  }
+}
+
+//absolute value
+public class Main {
+  public static void main(String[] args) {
+    System.out.println(Math.abs(-4.7));  
+  }
+}
+
+//random (0,1)
+public class Main {
+  public static void main(String[] args) {
+    System.out.println(Math.random());  
+  }
+}
+
+*/
