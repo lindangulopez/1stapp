ee2c2f5abe282aa48414a5a20aa76e21403ae0e5
LINDA ANGULO LOPEZ <43523986+lindangulopez@users.noreply.github.com>
2020-11-26 10:47:51 +0100

    Part4 ?

--- StringsFirstAssignments/Part4.java
+++ StringsFirstAssignments/Part4.java
@@ -1,4 +1,21 @@
-URLResource file = new  URLResource("http://someURL");
+public class URLFINDER
+{
+    public void asd(){
+        URLResource file = new URLResource("http://www.dukelearntoprogram.com/course2/data/manylinks.html");
+        for ( String item : file.words() ) {
+            String itemLower = item.toLowerCase();
+            int pos = itemLower.indexOf("youtube.com");
+            if ( pos != -1 ) {
+                int beg = item.lastIndexOf("\"", pos);
+                int end = item.indexOf("\"", pos + 1);
+                System.out.println(item.substring(beg + 1, end));
+        }
+    }
+  }
+}
+
+
+/* URLResource file = new  URLResource("http://someURL");
    	for (String item : file.words()) {
        	   String itemLower = item.toLowerCase();
        	   int pos = itemLower.indexOf("youtube.com");
@@ -10,3 +27,4 @@ URLResource file = new  URLResource("http://someURL");
 
                }
    	}
+*/
